cf02118432ddb7a2f32b8bad3bd2e6aadf110bb7
Limoilou Renaud <60702430+larenaud@users.noreply.github.com>
2021-09-27 15:59:19 -0400

    codes to reproduce figures in main text + supplementary

---
+++ R/04_finalFigs.R
@@ -0,0 +1,353 @@
+library(ggplot2)
+library(ggthemes)
+library(pander)
+library(cowplot)
+library(tidyverse)
+library(forcats)
+library(ggpubr)
+library(mgcv)
+library(gamm4)
+library(lme4)
+
+
+# ggplot custom  ----------------------------------------------------------
+theme_set(theme_bw())
+scale_colour_discrete <- function(...,palette="Set1") {
+	scale_colour_brewer(...,palette=palette)
+}
+scale_colour_orig <- ggplot2::scale_colour_discrete
+scale_fill_discrete <- function(...,palette="Set1") {
+	scale_fill_brewer(...,palette=palette)
+}
+
+
+# load data ---------------------------------------------------------------
+setwd("/Users/LimoilouARenaud/Documents/Scolarite/PostDocI/Mismatch")
+load("Cache/20210426revisedDf.RData")
+
+dat.trend <- fall.pheno.ind[, 1:8] %>% left_join(spring.pheno.ind)
+#dat.trend$mismatch <- dat.trend$birthdateJJ - dat.trend$evi_log_up_jul
+# REVISIONS ADDED EVI_TM1
+dat.trend.yr <- dat.trend[, c("year", "spring_temp", "fall_temp", "evi_log_up_jul",  "evi_tm1","birthdate")] %>%
+	group_by(year) %>%
+	summarise_all(mean)
+
+
+# Figure 1 main ms, panels a-f ---------------------------------------------------------
+mod.ts.l <- lm(spring_temp~year,data=unique(dat.trend[, c("year", "spring_temp")]) )
+mod.tf.l <- lm(fall_temp~year,data=unique(dat.trend[, c("year", "fall_temp")]) )
+
+dat.trend.yr$ts.l=predict(mod.ts.l, data=data.trend.yr)
+dat.trend.yr$tf.l=predict(mod.tf.l, data=data.trend.yr)
+tf.l.ci.l=predict(mod.tf.l, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+tf.l.ci=data.frame(year = seq(2001, 2017, by=0.1), y=tf.l.ci.l$fit, ci.h=tf.l.ci.l$se.fit*1.96+tf.l.ci.l$fit, ci.l=-tf.l.ci.l$se.fit*1.96+tf.l.ci.l$fit)
+
+ts.l.ci.l=predict(mod.ts.l, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+ts.l.ci=data.frame(year = seq(2001, 2017, by=0.1), y=ts.l.ci.l$fit, ci.h=ts.l.ci.l$se.fit*1.96+ts.l.ci.l$fit, ci.l=-ts.l.ci.l$se.fit*1.96+ts.l.ci.l$fit)
+
+
+g.ts <- ggplot(dat.trend.yr, aes(x=year))+
+	geom_point(aes(y=spring_temp), alpha=0.3, size =2) + geom_line(aes(y=spring_temp), alpha=0.3)+
+	# geom_path(data = ts.l.ci, aes(y=y), color="black") +
+	#geom_ribbon(data=ts.l.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) + 
+	labs(x="Year", y="Spring temperature \n (°C)")+ 
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+
+g.tf <- ggplot(dat.trend.yr, aes(x=year))+
+	geom_point(aes(y=fall_temp), alpha=0.3, size =2) +
+	geom_path(data = tf.l.ci, aes(y=y)) +
+	geom_ribbon(data=tf.l.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) + 
+	labs(x="Year", y="Autumn temperature \n (°C)")+ 
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+#cowplot::plot_grid(g.tf, g.ts)
+
+mod.gu.l <- lm(evi_log_up_jul~year,data=unique(dat.trend[, c("year", "evi_log_up_jul")]) )
+
+up.l.ci.l=predict(mod.gu.l, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+up.l.ci=data.frame(year = seq(2001, 2017, by=0.1), y=up.l.ci.l$fit, ci.h=up.l.ci.l$se.fit*1.96+up.l.ci.l$fit, ci.l=-up.l.ci.l$se.fit*1.96+up.l.ci.l$fit)
+
+g.up.yr <- ggplot(dat.trend.yr, aes(x=year))+
+	geom_point(aes(y=evi_log_up_jul), alpha=0.3, size =2) +
+	#geom_point(aes(y=pred.gu), colour="turquoise") + #for predeicted gu
+	geom_path(data = up.l.ci, aes(y=y), color="black") +
+	geom_ribbon(data=up.l.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) +
+	labs(x="Year",y="Green-up date (Julian day)")+
+	ylim(123,170)+ 
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+
+mod.gd.gam <- gam(evi_tm1~s(year),gamma = 1.4, data=unique(dat.trend[, c("year", "evi_tm1")]) ) # Woods 
+do.l.ci.l=predict(mod.gd.gam, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+do.l.ci=data.frame(year = seq(2001, 2017, by=0.1), y=do.l.ci.l$fit, ci.h=do.l.ci.l$se.fit*1.96+do.l.ci.l$fit, ci.l=-do.l.ci.l$se.fit*1.96+do.l.ci.l$fit)
+
+g.gd.yr <- ggplot(dat.trend.yr, aes(x=year))+
+	geom_point(aes(y=evi_tm1), alpha=0.3, size =2) + geom_line(aes(y=evi_tm1), alpha=0.3)+
+	#geom_point(aes(y=pred.gu), colour="turquoise") + #for predeicted gu
+	#geom_path(data = do.l.ci, aes(y=y), color="black", linetype = "dashed") +
+	#	geom_ribbon(data=do.l.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) +
+	labs(x="Year",y="Brown-down date \n (Julian day)")+
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+
+mod.bd.gam<- gamm4(birthdate ~ s(year), random=~(1|mom_id), REML=T, data=dat.trend)
+
+pred=predict(mod.bd.gam$gam, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+pred.ci=data.frame(year = seq(2001, 2017, by=0.1), y=pred$fit, ci.h=pred$se.fit*1.96+pred$fit, ci.l=-pred$se.fit*1.96+pred$fit)
+
+g.bd.yr <- ggplot(dat.trend.yr, aes(x=year))+
+	geom_point(aes(y=birthdate), alpha=0.3, size =2) +
+	#=geom_point(data=dat.trend.yr, aes(y=pred.bd), color="turquoise") +
+	#geom_point(aes(y=pred.gu), colour="turquoise") + #for predeicted gu
+	# geom_path(data = pred2.ci, aes(y=y), linetype = "dotted") +
+	#geom_ribbon(data=pred2.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) +
+	geom_path(data = pred.ci, aes(y=y)) +
+	geom_ribbon(data=pred.ci, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) +
+	ylim(123,175) + 
+	labs(x='Year ',
+			 y='Parturition date \n (Julian day)')+ 
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+# REVISIONS - ADD TEMPORAL TRENDS IN GUP AND BDOWN
+p <- cowplot::plot_grid(g.ts, g.tf.gam, g.up.yr, g.gd.yr, g.bd.yr,ncol=2, nrow=3,labels=c("a)", "b)", "c)", "d)", "e)"), align = 'v')
+#ggsave("FIG_S3_gamsBDGU.png", width = 140, height = 140, units = 'mm')
+
+# Fig 1f mismatch- both gamm + poly on figure 
+mod.mis.gam2 <- gamm4(mismatch~s(year), random=~(1|mom_id), REML=T, data=dat.trend)
+pred=predict(mod.mis.gam2$gam, newdata =data.frame(year = seq(2001, 2017, by=0.1)), se.fit=T)
+pred.ci.mismatch=data.frame(year = seq(2001, 2017, by=0.1), y=pred$fit, ci.h=pred$se.fit*1.96+pred$fit, ci.l=-pred$se.fit*1.96+pred$fit)
+
+mod.mis.l <- lmer(mismatch~year +I(year^2) + (1|mom_id), REML=T, data=dat.trend)
+pred=predict(mod.mis.l, newdata =data.frame(year = seq(2001, 2017, by=0.1)),re.form=NA)
+fun=function(x){predict(x, newdata=data.frame(year=seq(2001, 2017, by=0.1)), re.form=NA)}
+pred=bootMer(mod.mis.l, FUN=fun, nsim = 10)
+
+pred.ci.mis=data.frame(year = seq(2001, 2017, by=0.1), 
+											 y=colMeans(pred$t), 
+											 ci.h=apply(pred$t, 2, quantile, 0.025), 
+											 ci.l=apply(pred$t, 2, quantile, 0.975))
+
+
+g.mis <- ggplot(dat.trend, aes(x=year))+
+	geom_point(aes(y=mismatch), alpha =0.2) + 
+	geom_path(data = pred.ci.mis, aes(y=y), color="turquoise") +
+	geom_ribbon(data=pred.ci.mis, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2, fill = "turquoise") + 
+	geom_path(data = pred.ci.mismatch, aes(y=y)) +
+	geom_ribbon(data=pred.ci.mismatch, aes(ymin = ci.l, ymax = ci.h), alpha = 0.2) +
+	labs(x="Year", y ="Mismatch \n (number of days)")+ 
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+# REVISIONS - ADD TEMPORAL TRENDS IN GUP AND BDOWN
+FIG1_multi= cowplot::plot_grid(g.ts, g.tf.gam, g.up.yr, g.gd.yr, g.bd.yr,g.mis, 
+															 ncol=2, nrow=3,
+															 labels=c("a)", "b)", "c)", "d)", "e)", 'f)'),
+															 align = 'v', rel_widths = c(1,1))
+#cowplot::save_plot("Graphs/revisions2/FIG1_multi_revised.png", FIG1_multi,
+#									 ncol = 2, #
+#									 nrow = 3, #
+									 # each individual subplot should have an aspect ratio of 1.3
+#									 base_aspect_ratio = 1.3)
+
+
+# Figure 2  ---------------------------------------------------------------
+rm(list = ls())
+load("Cache/finalDF.RData")
+
+m10 <- gamm4(mismatch ~ s(spring_temp, k=10) + s(fall_prec, k=10), random = ~ (1|mom_id) + (1|year), data = dt4)
+summary(m10$gam)
+summary(m10$mer)
+
+newdat <- data.frame(spring_temp=seq(min(dt4$spring_temp), max(dt4$spring_temp), length = 100),
+										 fall_prec=mean(dt4$fall_prec, na.rm = T), 
+										 mother_id="A44",
+										 year="2010")
+predictions = predict(m10$gam, newdata=newdat, se.fit = TRUE) # previously m1 but too circular... 
+
+# Consolidating new data and predictions
+newdat = cbind(newdat, predictions)
+
+# If you want CIs 
+newdat <- within(newdat, {
+	lower = fit-1.96*se.fit
+	upper = fit+1.96*se.fit
+})
+
+# Plot, for example, the predicted outcomes as a function of x1...
+p1 <- ggplot(newdat, aes(x=spring_temp, y=fit)) + 
+	geom_point(data=dt4, aes(x = spring_temp, y = mismatch), colour = "grey") +
+	geom_line() +
+	geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.4)+ 
+	ylab("Mismatch (number of days)")+ xlab("Spring temperature (°C)")+
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+
+p1
+
+newdat <- data.frame(fall_prec=seq(min(dt4$fall_prec), max(dt4$fall_prec), length = 100),
+										 spring_temp =mean(dt4$spring_temp, na.rm = T), 
+										 mother_id="A44",
+										 year="2010")
+predictions = predict(m10$gam, newdata=newdat, se.fit = TRUE) # previously m1 but too circular... 
+
+# Consolidating new data and predictions
+newdat = cbind(newdat, predictions)
+
+# If you want CIs 
+newdat <- within(newdat, {
+	lower = fit-1.96*se.fit
+	upper = fit+1.96*se.fit
+})
+
+# Plot, for example, the predicted outcomes as a function of x1...
+p2 <- ggplot(newdat, aes(x=fall_prec, y=fit)) + 
+	geom_point(data=dt4, aes(x = fall_prec, y = mismatch), colour = "grey") +
+	geom_line() +
+	geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.4)+ 
+	ylab("Mismatch (number of days)")+ xlab("Autumn precipitation (mm)")+
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
+p2
+
+FIG2_mismatch= cowplot::plot_grid(p1,p2,
+																	ncol=2,
+																	align = "vh",
+																	labels = c("a)", "b)"),
+																	rel_widths = c(1,1))
+
+# cowplot::save_plot("Graphs/revisions2/FIG2_mismatch_revised_2.png", FIG2_mismatch,
+# 									 ncol = 2, #
+# 									 nrow = 1, #
+# 									 base_aspect_ratio = 1)
+
+
+# Figure 3 Lamb weaning mass ----------------------------------------------------------------
+library(partR2)
+wean$m9 <- lmer(weanMass ~ fallMass_tm1_z*(evi_im_z + I(evi_im_z^2)) + evi_pm_z + sex +   (1|mom_id) + (1|yr),data = dat.weaning, REML = T) # buffering hypothesis 
+wean$m10 <-lmer(weanMass ~ fallMass_tm1_z*evi_im_z + evi_pm_z + sex + (1|mom_id) + (1|yr),data = dat.weaning, REML = T) # buffering hypothesis 
+# res <- partR2(wean$m9, partvars = c("fallMass_tm1_z", "evi_im_z", "evi_pm_z", "sex", "fallMass_tm1_z:evi_im_z"), nboot=100)
+# res <- partR2(wean$m3, partvars = c("evi_im_z", "evi_pm_z", "sex"), nboot=300)
+
+summary(wean$m10)
+# mom_id   (Intercept) 1.777    1.333   
+# yr       (Intercept) 5.114    2.262   
+# Residual             5.533    2.352  
+# 
+# Fixed effects:
+# 	Estimate Std. Error t value
+# (Intercept)              26.4056     0.6371  41.450
+# fallMass_tm1_z            1.1527     0.2717   4.243
+# evi_im_z                 -3.1605     0.2405 -13.143
+# evi_pm_z                 -0.5049     0.6071  -0.832
+# sexmale                   2.0341     0.4116   4.942
+# fallMass_tm1_z:evi_im_z  -0.1446     0.1882  -0.768
+
+t=round(summary(wean$m10)$coef, 3) # 
+t=kable(t) %>%
+	kable_styling(font_size = 10) %>%
+	kable_styling("bordered") %>%
+ #save_kable(file = "Graphs/TableS5_weaning_A3.html", self_contained = T)
+
+
+
+MuMIn::r.squaredGLMM(wean$m10) # the first 
+# R2m       R2c
+# 0.4628682 0.7607955
+
+MuMIn::r.squaredGLMM(wean$m9)
+# R2m       R2c
+# [1,] 0.4670302 0.7651987
+
+
+str(dat.weaning)
+newd <- data.frame()
+nd <- expand.grid(evi_im_z=seq(-1.7, 4.2, length = 100), 
+									fallMass_tm1_z = mean(dat.weaning$fallMass_tm1_z, na.rm =T), # without interaction since NS
+									evi_pm_z= mean(dat.weaning$evi_pm_z, na.rm=T),
+									sex = "male")
+newdWM <- rbind(newd,nd)
+# compared t0 
+min(dat.weaning$evi_im) #  -18.7619
+max(dat.weaning$evi_im) #  43.33333
+min(dat.weaning$evi_im_z) #  -1.514306
+max(dat.weaning$evi_im_z) #   3.49751
+sd(dat.weaning$evi_im)
+
+
+myfun <- function(x) predict(x,newdata=newdWM,type="response",re.form=NA) # removed allow.new.levels=T
+
+boo <- bootMer(m10, myfun, 1000, seed = NULL, use.u = FALSE, re.form=NA,
+							 verbose = T)
+
+boo <- data.frame(boo)
+str(boo)
+
+newdWM$predi <- apply(boo, 2, mean) # removed boo$t because not in seq of x anymore
+newdWM$max <- apply(boo, 2, function(x) quantile(x, 0.975))
+newdWM$min <- apply(boo, 2, function(x) quantile(x, 0.025))
+
+# # pour 'déscaler'
+newdWM$evi_im<-(newdWM[,"evi_im_z"]*q.SD.IM) + q.MEAN.IM
+
+# prediction
+IM<- ggplot(newdWM, aes(y=predi, x = evi_im)) +
+	geom_ribbon(aes(ymin = min, ymax = max),  colour=NA, alpha=0.2) + 
+	geom_line(aes(y = predi)) + 
+	geom_point(data=dat.weaning, aes(y=weanMass, x=evi_im), alpha = .5) + 
+	labs(x=expression('Individual mismatch (days)')) + 
+	labs(y=expression('Lamb weaning mass (kg)')) +  
+	#scale_x_continuous(limits = c(-2, 4),breaks=seq(-2, 4,1)) +
+	# theme_pander(12) +
+	theme(legend.title=element_blank(), legend.position = c(0.3, 0.4)) +
+	theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) 
+
+# population mismatch
+
+str(dat.weaning)
+newd <- data.frame()
+nd <- expand.grid(evi_pm_z=seq(min(dat.weaning$evi_pm_z, na.rm = T), 
+															 max(dat.weaning$evi_pm_z, na.rm =T), length = 100), 
+									fallMass_tm1_z = mean(dat.weaning$fallMass_tm1_z, na.rm =T), 
+									evi_im_z= mean(dat.weaning$evi_im_z, na.rm=T),
+									#fem_z = mean(dat.weaning$fem_z, na.rm = T), 
+									sex = "female")
+newdWM <- rbind(newd,nd)
+
+myfun <- function(x) predict(x,newdata=newdWM,type="response",re.form=NA) # removed allow.new.levels=T
+
+boo <- bootMer(m10, myfun, 1000, seed = NULL, use.u = FALSE, re.form=NA,
+							 verbose = T)
+
+boo <- data.frame(boo)
+str(boo)
+
+newdWM$predi <- apply(boo, 2, mean) # removed boo$t because not in seq of x anymore
+newdWM$max <- apply(boo, 2, function(x) quantile(x, 0.975))
+newdWM$min <- apply(boo, 2, function(x) quantile(x, 0.025))
+
+# # pour 'déscaler'
+newdWM$evi_pm<-(newdWM[,"evi_pm_z"]*q.SD.PM) + q.MEAN.PM
+
+# prediction
+PM<- ggplot(newdWM, aes(y=predi, x = evi_pm)) +
+	geom_ribbon(aes(ymin = min, ymax = max),  colour=NA, alpha=0.2) + 
+	geom_line(aes(y = predi)) + 
+	geom_point(data=fitness.data, aes(y=weanMass, x=evi_pm), alpha = .5) + # perhaps take dat.weaning??? 
+	labs(x=expression('Population mismatch (days)')) + 
+	labs(y=expression('Lamb weaning mass (kg)')) 
+#scale_x_continuous(limits = c(-2, 4),breaks=seq(-2, 4,1)) +
+# theme_pander(12) +
+#theme(legend.title=element_blank(), legend.position = c(0.3, 0.4)) +
+#theme(plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) 
+
+# panel 
+library(cowplot)
+plot <- plot_grid(IM, PM, labels = c("a)", "b)"), align = 'vh')
+
+ggsave("Graphs/revisions2/FIG3PanelWeanMass_1.png", width = 150, height = 100, units="mm")
+save_plot("Graphs/revisions2FIG3PanelWeanMass_2.png", plot,
+					ncol = 2, #
+					nrow = 1, #
+					# each individual subplot should have an aspect ratio of 1.3
+					base_aspect_ratio = 1
+)
+
+
