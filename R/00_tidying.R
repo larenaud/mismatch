878633b81d6c306dacdfc2a6d21be3b85d5edf97
Limoilou Renaud <60702430+larenaud@users.noreply.github.com>
2021-10-04 11:26:37 -0400

    added tidying mismatch from raw data

--- R/00_tidying.R
+++ R/00_tidying.R
@@ -29,18 +29,13 @@ library(readxl)
 getwd()
 rm(list = ls())
 
+# create fitness dataframe from raw data ------------------------------------------
 
-# ADD TIDYING STEPS FOR THIS DF - TO BE ADDED ON THE README.TXT 
-# CAN BE FOUND IN REV 1 FILE
-
-
-# create dataframe from raw data ------------------------------------------
-
-load("Cache/neonatalData.RData")
+load("cache/neonatalData.RData")
 table(surv_ind$yr, surv_ind$neonatal)
 
 # include birthdates to individual data -----------------------------------------------------
-tidybdates<- read.csv("Data/tidybdates_noneonatal.csv") # 
+tidybdates<- read.csv("data/mine/tidybdates_noneonatal.csv") # 
 head(tidybdates)
 
 summary(tidybdates$birthdate) # med = 150
@@ -73,10 +68,9 @@ head(tmp1)
 
 # merge birthdates of neonatal to this tmp1 file 
 
-neonatal_dates <- read.csv("Data/neonatal_dates.csv")
+neonatal_dates <- read.csv("data/mine/neonatal_dates.csv")
 head(neonatal_dates)
 summary(neonatal_dates$birthdate)
-
 
 names(neonatal_dates)
 neonatal_dates <- neonatal_dates %>% 
@@ -104,7 +98,6 @@ names(tmp1)= c("yr","mom_id","wt12","fallMass","newRS","prs", "sex","fallMass_tm
 names(tmp2)= c("yr","mom_id","wt12","fallMass","newRS","prs","sex", "fallMass_tm1","pred","fem","pred_tm1","fem_tm1","total","neonatal", "weanMass", "lamb.surv.t1", "lamb_id","birthdate","eviUP")
 tmp2 = rbind(tmp1, tmp2) # n = 522 # now 454
 tmp2<-tmp2[!is.na(tmp2$birthdate),] %>% arrange(yr, mom_id) # n= 600+# NO NOW n=281 2020-08-06
-
 
 # verif
 tmp1 %>%
@@ -144,45 +137,34 @@ colnames(surv_bdate) <- c("yr","mom_id","springMass","fallMass","newRS","prs","s
                           "lambSurv_t1","neonatal","eviUP")
 
 # verification and basic stat for ms  ------------------------------------------------------
+
 # verification 
 Ram_mass <- read.csv("~/Documents/PhD/Analyses/OWPC/OWPC/data/Ram_mass.csv")
 colnames(surv_ind)
 tmp <-  surv_ind[, c("yr","mom_id","age","wt12","fallMass", "neonatal","lamb_id","weanMass")]
-
 tmp_lb <- merge(tmp,
                 Ram_mass, 
                 by.x = c("yr", "lamb_id"), 
                 by.y=c("yr", "ID"))
 
 # ok for some reasons lots of lambs which survived the neonatal stage could not have their mass estimated.
-
-
-# verification 
 table(surv_bdate$yr, surv_bdate$neonatal)
-
 median(surv_bdate$birthdate, na.rm = T) # 150 total 
-
 median(surv_bdate$birthdate[surv_bdate$neonatal == "0"],na.rm = T) # 153 - June 2 julian calendar  
 median(surv_bdate$birthdate[surv_bdate$neonatal == "1"],na.rm = T) # 149 - 29 may
-
-
 # end verification 
 
 # calculate mismatch ------------------------------------------------------
 # index available for year 2000-2001
-
 # took birthdate, not conception and reversed the subtraction # reed et al. 2013
 
 surv_bdate$mismatch <- as.Date(surv_bdate$date) - as.Date(surv_bdate$eviUP)
 
-
 # calculate individual timing  ----------------------------------------------
-
 surv_bdate <- surv_bdate %>%
     group_by(yr) %>%
     mutate(median = median(birthdate, na.rm =T), 
            timing = birthdate - median)
-
 
 # clean up file for analyses 
 colnames(surv_bdate)
@@ -192,20 +174,15 @@ surv_bdate <- surv_bdate[, c("yr","mom_id","neonatal","weanMass", "lambSurv_t1",
                              "mismatch", "timing")] # removed indices 2 and 3 may 20 2020
 
 rm(tmp, tmp1, tmp2)
-# prepare for analyses 
 
+# prepare for analyses 
 colnames(surv_bdate)
 head(surv_bdate) # with birthdates and conception dates 
-#surv_bdate$index1_gpp= as.integer(surv_bdate$index1_gpp)
-#surv_bdate$index1_snow= as.integer(surv_bdate$index1_snow)
-#surv_bdate$index1_ndvi= as.integer(surv_bdate$index1_ndvi)
 surv_bdate$mismatch= as.integer(surv_bdate$mismatch)
 surv_bdate$timing= as.integer(surv_bdate$timing)
 
 # keep timing as number of days
-
 surv_bdate$yr <- as.factor(as.character(surv_bdate$yr))
-
 str(surv_bdate)
 surv_bdate<- as.data.frame(surv_bdate)
 surv_bdate$pred_tm1 <- as.factor(surv_bdate$pred_tm1)
@@ -213,7 +190,6 @@ surv_bdate$pred <- as.factor(surv_bdate$pred)
 surv_bdate$neonatal <- as.factor(surv_bdate$neonatal)
 surv_bdate$prs <- as.factor(surv_bdate$prs)
 surv_bdate$prs<-droplevels(surv_bdate$prs)
-
 surv_bdate$newRS <- as.factor(surv_bdate$newRS)
 surv_bdate$lambSurv_t1 <- as.factor(surv_bdate$lambSurv_t1)
 surv_bdate$newRS <- as.factor(surv_bdate$newRS)
@@ -234,8 +210,6 @@ surv_bdate$newRS <- as.factor(surv_bdate$newRS)
 surv_bdate <- surv_bdate[!is.na(surv_bdate$yr),] #  284
 surv_bdate$yr<-droplevels(surv_bdate$yr)
 surv_bdate$mom_id<-droplevels(surv_bdate$mom_id)
-
-colnames(surv_bdate)
 
 # # modified apr 8 2021 in response to rev#2 comment to do sel analyses on bd only, not mismatch 
 # selection.data <- surv_bdate[, c("yr","ID","neonatal", "lambSurv_t1", "birthdate" )]
@@ -265,7 +239,7 @@ tmp_avg <- surv_bdate %>% # not inflated
               medBD = median(birthdate, na.rm = T))
 
 
-# here we define annual population mismatch (PM) as the arithmeic average of IM values each year
+# here we define annual population mismatch (PM) as the arithmetic average of IM values each year
 dt <- surv_bdate %>%
     dplyr::group_by(yr) %>% # la moyenne annuelle et on centre à cette moyenne annuelle
     dplyr::mutate(evi_pm= mean(mismatch, na.rm = T), evi_im = mismatch - evi_pm, # then IM is standardized relative to annual average... a within-year IM 
@@ -274,9 +248,8 @@ dt <- surv_bdate %>%
 plot(evi_pm~yr, data=dt)
 plot(evi_im~yr, data=dt)
 
-
-
 # detrend -----------------------------------------------------------------
+
 # detrending based on lm # ATTENTION SOME ARE ANNUAL VALUES THUS DUPLICATED
 
 dt1 = dt %>%
@@ -299,7 +272,6 @@ dt2 = dt %>%
 
 
 # detrend 
-
 evi_pm.r= resid(lm(evi_pm~as.numeric(as.character(yr)),data=dt2)) # this is equivalent to bdate since annual effect is removed - mismatch cancels out 
 fem.r=resid(lm(fem~as.numeric(as.character(yr)),data=dt2)) # this is equivalent to bdate since annual effect is removed - mismatch cancels out 
 fem_tm1.r=resid(lm(fem_tm1~as.numeric(as.character(yr)),data=dt2)) # this is equivalent to bdate since annual effect is removed - mismatch cancels out 
@@ -309,6 +281,7 @@ dt2 <- cbind(dt2, evi_pm.r, fem.r, fem_tm1.r, bd_pop.r)
 colnames(dt2) <- c( "yr","evi_pm","fem","fem_tm1", "bd_pop", "evi_pm.r","fem.r","fem_tm1.r", "bd_pop.r")
 
 dt1 = merge(dt1, dt2[, c("yr", "evi_pm.r","fem.r","fem_tm1.r", "bd_pop.r")], by.x="yr", by.y = "yr", all.x = T)
+
 # clean dataset 
 rm(male, RamMtnPop, surv, tmp, tmp2, tmp3, females, neonatal_dates,tmp_lb, surv_pop,popPheno, tidybdates, surv_pop, Ram_mass, pheno, neosurv, dt)
 View(t1<-table(dt1$mom_id)) # 80 females total (NA removed in environment)
@@ -331,7 +304,7 @@ View(mom_id<-table(dt1$mom_id))  # now 80 but some are NAs
 dt1$prs <- as.factor(dt1$prs)
 dt1$yr<- as.factor(as.character(dt1$yr))
 
-
+# ensure numeric
 colnames(dt1)
 cols.num <-c("bd.r", "evi_pm.r", "evi_im.r","fem.r",
              "fem_tm1.r","weanMass.r","mass_tm1.r")
@@ -340,110 +313,38 @@ sapply(dt1, class)
 
 fitness.data <- dt1
 
-#save(fitness.data, file = "Cache/20210426fitnessData_centeredDetrended.RData")
+# saving fitness data as R object -----------------------------------------
+# save(fitness.data, file = "Cache/20210426fitnessData_centeredDetrended.RData")
 
-
-
-# basic stats and verification  -------------------------------------------------------------
+# verification  -------------------------------------------------------------
 colnames(dt1)
-
 hist(dt1$evi_pm) # OK
 hist(dt1$evi_pm.r) # OK
-
-mean(dt1$evi_pm.r) # there's a problem - too small value ?!! OK now =probably caused by regressing over duplicated years 
+mean(dt1$evi_pm.r) # OK now =probably caused by regressing over duplicated years 
 mean(dt1$evi_pm)
 mean(dt1$evi_im)
 mean(dt1$evi_im.r)
 
-dt1$ID %>% 
+dt1$mom_id %>% 
     droplevels() %>% 
     table %>% 
     mean() # on average 3.5125 per female  - did not exclude any female , starting 2000
-str(dt1$ID) # 69 niveaux 
 
-
-
-View(t1<-table(dt1$ID)) # de 1 à 12 captures 
-dt1$ID<- droplevels(dt1$ID) # n = 69 females
-
+View(t1<-table(dt1$mom_id)) #  1 - 12 captures 
+dt1$mom_id<- droplevels(dt1$mom_id) # n = 69 females
 
 # compare to "Raw"
-SURV20200608 <- read.csv("~/Documents/PhD/Analyses/Mismatch/Data/SURV20200608.csv")
-raw_df <- SURV20200608
-
-raw_df <- raw_df[raw_df$yr>1999,] # n= 714
-
-lambs <- raw_df[raw_df$age==0,]
-code0 <- raw_df[raw_df$code.sr %in% "0",] #n = 148
-code1 <- raw_df[raw_df$code.sr %in% "1",] #n = 61
-
-code2 <- raw_df[raw_df$code.sr %in% "2",] %>% select(c("yr", "ID", "wt12", "wt114")) %>%arrange(yr)#n = 38 
-print(code2)
-# yr  ID     wt12    wt114
-# 1  2000 37T 50.89435 70.80708
-# 2  2000 51T 55.54690 71.92865
-# 3  2000 28U 57.87515 74.95261
-# 4  2001 28W 59.46933 69.94026
-# 5  2001 28T 57.06784 70.64924
-# 6  2001 A44 60.99539 73.43043
-# 7  2001 42U 67.47750 74.47740
-# 8  2001 A50 58.87118 68.97804
-# 9  2002 E13 42.09051 61.90885
-# 10 2002 A35 56.19937 63.53771
-# 11 2003 A44 63.96367 76.31414
-# 12 2003 A35 53.58069 66.20738
-# 13 2004 A44 64.32383 81.62945
-# 14 2004 G14 60.48006 74.70594
-# 15 2004  F1 58.36308 73.79375
-# 16 2005  D8 56.83168 69.80055
-# 17 2005 A35 60.07749 69.34564
-# 18 2007 C14 51.33252 63.35377
-# 19 2008 C14 48.59432 69.44156
-# 20 2008 E12 61.38846 82.41512
-# 21 2009  K1 53.18254 68.79174
-# 22 2010 M28 42.02237 62.34650
-# 23 2010  J5 59.40132 79.03256
-# 24 2012  N4 50.87282 66.02334
-# 25 2012  J5 63.86907 76.28546
-# 26 2012  N6 64.33342       NA
-# 27 2013  M7 58.86456 70.82768
-# 28 2013 M15 57.45078 70.52497
-# 29 2013  J5 62.76116 75.18145
-# 30 2013  L2 62.26739 74.43463
-# 31 2013 P13 57.54786 69.43493
-# 32 2015 P11 52.47062 69.55153
-# 33 2015  O6 57.53812 74.88706
-# 34 2016 S10 46.24042 60.61860
-# 35 2017  U7 51.27235 72.77973
-# 36 2018 M28 52.61488 65.15623
-# 37 2018  L2 62.84172 77.41216
-# 38 2018  P3 52.61533 68.18099
-
-colnames(fitness.data)
-na.fit <- fitness.data[fitness.data$weanMass %in% NA & fitness.data$neonatal %in% "1",] %>% select(c("yr", "mom_id", "weanMass", "fallMass", "fallMass_tm1")) %>% arrange(yr)#n = 38 
-print(na.fit)
-# yr    ID    weanMass fallMass fallMass_tm1
-# <fct> <fct>    <dbl>    <dbl>        <dbl>
-# 1 2000  28U         NA     75.0         74.7
-# 2 2000  37T         NA     70.8         68.5
-# 3 2000  51T         NA     71.9         69.7
-# 4 2001  28T         NA     70.6         71.8
-# 5 2001  A33         NA     70.4         73.2
-# 6 2001  A44         NA     73.4         76.3
-# 7 2002  A35         NA     63.5         67.9
-# 8 2003  A35         NA     66.2         63.5
-# 9 2003  A44         NA     76.3         74.6
-# 10 2004  E12         NA     74.5         72.0
-# # … with 27 more rows
-
-
-
-
-
-
-
-
-
+# SURV20200608 <- read.csv("~/Documents/PhD/Analyses/Mismatch/Data/SURV20200608.csv")
+# raw_df <- SURV20200608
+# raw_df <- raw_df[raw_df$yr>1999,] # n= 714
+# lambs <- raw_df[raw_df$age==0,]
+# code0 <- raw_df[raw_df$code.sr %in% "0",] #n = 148
+# code1 <- raw_df[raw_df$code.sr %in% "1",] #n = 61
+# code2 <- raw_df[raw_df$code.sr %in% "2",] %>% select(c("yr", "ID", "wt12", "wt114")) %>%arrange(yr) #n = 38 
+# print(code2)
+# colnames(fitness.data)
+# na.fit <- fitness.data[fitness.data$weanMass %in% NA & fitness.data$neonatal %in% "1",] %>% select(c("yr", "mom_id", "weanMass", "fallMass", "fallMass_tm1")) %>% arrange(yr)#n = 38 
+# print(na.fit)
 
 # load fitness data and parturition date for later --------------------------------------
 load("cache/20210426fitnessData_centeredDetrended.RData")
@@ -627,8 +528,6 @@ names(spring.pheno) <- c("year", "spring_prec", "spring_temp")
 
 
 # include spring phenology (same year) ------------------------------------
-
-
 # get spring variables 
 pheno_ram <- read_csv("data/mine/pheno_ram.csv")
 spring.pheno$year <- as.numeric(as.character(spring.pheno$year))
@@ -640,7 +539,6 @@ medBD <- fitness.data %>% group_by(year) %>% summarise(medBD = median(birthdate)
 spring.pheno.pop <- spring.pheno%>%left_join(medBD) %>% left_join(pheno_ram[, c(1, 18)]) %>% arrange(year) %>% na.omit()
 
 spring.pheno.ind <- fitness.data[c("year", "mom_id", "birthdate", "mismatch")] %>% left_join(spring.pheno) %>% left_join(pheno_ram[, c(1, 18)]) %>% arrange(year, mom_id) %>% na.omit()
-
 
 # detrend spring ----------------------------------------------------------
 
@@ -656,12 +554,8 @@ detrended.spring.pheno <- spring.pheno.pop %>%
            springT.r = resid(lm(spring_temp ~ year)),
            evi_GU.r = resid(lm(evi_log_up_jul ~ year)))
 
-
 # clean
-
 rm(spring.precip, spring.pheno, spring.temp, rs, s)
-
-
 
 # re-extract fall weather based on sliding window ---------------------------------
 
@@ -697,7 +591,6 @@ fitness.data%>%
 # do some quick verification and compare to plasticity data here 
 # looks fine
 # end verification 
-
 
 # create two df : one pop and one ind -------------------------------------
 # merge data to birthdates 
@@ -755,8 +648,6 @@ rm(sims, d,dNA, dt2, dt3, dt4,pheno_ram, precip, temp, tmp1j, curDT, curDT2, Bir
 # file = "cache/20210426revisedDf.RData")
 
 
-
-
 # prepare export for Dryad ------------------------------------------------
 # load("cache/20210426revisedDf.RData") 
 
@@ -801,10 +692,9 @@ fitness.data$lamb_id <- as.numeric(fitness.data$lamb_id)
 fitness.data$lamb_id  <- as.factor(as.character(fitness.data$lamb_id))
 
 
-# save as csv for Dryad and analyses
-
-# CHANGE TO FITNESS DATA
+# save as csv for Dryad and analyses ------------------------------------
 
 # write.csv2(dat.trend, file = "data/mine/trends_data_noID.csv", row.names = FALSE)
 # write.csv2(fitness.data, file = "data/mine/mismatch_data_noID.csv", row.names = FALSE)
+
 
